7948b52d2b593c000cf824707c9f84616b538b1a
asimov <seyranmustafa4@gmail.com>
2025-04-26 00:39:27 +0300

    p

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,9 @@
-
-
-
 function App() {
- 
-
   return (
-    <div>
-      hello world
-      
+    <div className="h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 ">
+      <h1 className="text-3xl font-semibold text-center text-gray-300">Login</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
