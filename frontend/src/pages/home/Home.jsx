212dfa03bbdd56230685bba82f9979563b261c00
asimov <seyranmustafa4@gmail.com>
2025-04-26 01:23:04 +0300

    p

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,8 @@
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 text-danger'>
+			hello world
 			{/* <Sidebar /> */}
 			{/* <MessageContainer /> */}
 		</div>
