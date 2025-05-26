1ecac1e548200ee3856212d434150c79abfaa576
asimov <seyranmustafa4@gmail.com>
2025-05-26 21:02:44 +0300

    p

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-// import { AuthContextProvider } from "./context/AuthContext.jsx";
-// import { SocketContextProvider } from "./context/SocketContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			{/* <AuthContextProvider> */}
-				{/* <SocketContextProvider> */}
+			<AuthContextProvider>
+				<SocketContextProvider>
 					<App />
-				{/* </SocketContextProvider> */}
-			{/* </AuthContextProvider> */}
+				</SocketContextProvider>
+			</AuthContextProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
